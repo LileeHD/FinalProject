6fe3d57a5495ae7bd08b2b119e25b27766b91978
LileeHD <lileetoile@gmail.com>
2019-09-11 15:17:11 +0200

    activity_main doesn't display ads

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -0,0 +1,27 @@
+package com.udacity.gradle.builditbigger.free;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "HAMMER DOWN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Log.v(TAG, "PUB VISIBLE");
+            }
+        });
+    }
+}
