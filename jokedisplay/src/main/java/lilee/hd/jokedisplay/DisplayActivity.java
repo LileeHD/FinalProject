72761d38a8a7a1bcf75035d13e81cc73a9a14513
LileeHD <lileetoile@gmail.com>
2019-09-14 23:17:04 +0200

    09/04/2019 commit

--- jokedisplay/src/main/java/lilee/hd/jokedisplay/DisplayActivity.java
+++ jokedisplay/src/main/java/lilee/hd/jokedisplay/DisplayActivity.java
@@ -1,12 +1,14 @@
 package lilee.hd.jokedisplay;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class DisplayActivity extends AppCompatActivity {
     public static final String JOKE_EXTRA = "joke";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +17,10 @@ public class DisplayActivity extends AppCompatActivity {
         TextView jokeTextView = findViewById(R.id.joke_tv);
         String joke = getIntent().getStringExtra(JOKE_EXTRA);
 
-        if (joke !=null){
+        if (joke != null) {
             jokeTextView.setText(joke);
-        }else jokeTextView.setText(R.string.no_jokes_found);
+        } else jokeTextView.setText(R.string.no_jokes_found);
+
     }
 
 }
--- jokesprovider/src/main/java/lilee/hd/jokesprovider/JokesProvider.java
+++ jokesprovider/src/main/java/lilee/hd/jokesprovider/JokesProvider.java
@@ -2,7 +2,6 @@ package lilee.hd.jokesprovider;
 
 public class JokesProvider {
     public String getJoke() {
-//        return null;
         return "Why can't you hear Baptiste when he goes to the bathroom?\n" +
                 "The \"p\" is silent.";
     }
